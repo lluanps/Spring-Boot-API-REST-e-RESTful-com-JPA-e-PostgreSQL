fbd36284b6434793e99c75d2e6f2cb89753a9095
Luan Pinheiro <lluanps@gmail.com>
2023-02-04 21:26:29 -0300

    buscando todos usuarios

--- apirest/src/main/java/api/rest/controller/IndexController.java
+++ apirest/src/main/java/api/rest/controller/IndexController.java
@@ -1,40 +1,42 @@
 package api.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.rest.model.Usuario;
+import api.rest.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
-	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity<Usuario> init() {
+	@Autowired
+	UsuarioRepository usuarioRepository;
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> init(@PathVariable(value = "id") Long id) {
 
-		Usuario usuario = new Usuario();
-		usuario.setId(50L);
-		usuario.setLogin("lluanps@gmail.com");
-		usuario.setNome("Luan Pinheiro da Silva");
-		usuario.setSenha("123");
-		
-		Usuario usuario2 = new Usuario();
-		usuario2.setId(50L);
-		usuario2.setLogin("lfasluanps@gmail.com");
-		usuario2.setNome("Lua Pinheiro da Silva");
-		usuario2.setSenha("1223");
-		
-		List<Usuario> usuarios = new ArrayList<>();
-		usuarios.add(usuario);
-		usuarios.add(usuario2);
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
 
-		return new ResponseEntity(usuarios, HttpStatus.OK);
+		return new ResponseEntity<Usuario>(usuario.get(), HttpStatus.OK);
 	}
+	
+	@GetMapping(value = "/")
+	public ResponseEntity<List<Usuario>> usuario() {
+		
+		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
 
 }
