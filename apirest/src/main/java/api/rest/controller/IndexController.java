a2c8a3bec23a01c49ab013a5f9344d4268b15dfd
Luan Pinheiro <lluanps@gmail.com>
2023-02-06 13:20:21 -0300

    Criado metodo para atualizar e deletar

--- apirest/src/main/java/api/rest/controller/IndexController.java
+++ apirest/src/main/java/api/rest/controller/IndexController.java
@@ -6,11 +6,14 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.rest.model.Usuario;
@@ -63,6 +66,18 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.CREATED);
 		
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario, @PathVariable Long id) {
+		Usuario usuarioAtualizado = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(usuarioAtualizado, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<String> deletar(@PathVariable Long id) {
+		usuarioRepository.deleteById(id);
+		return new ResponseEntity<String>("Usuário deletado", HttpStatus.OK);
 	}
 	
 	
