43013ca5b0f74a6237ac6478b0c215c634f3bf9d
Luan Pinheiro <lluanps@gmail.com>
2023-02-06 15:58:48 -0300

    Criado classe Telefone e relacionamento um para muitos

--- apirest/src/main/java/api/rest/model/Usuario.java
+++ apirest/src/main/java/api/rest/model/Usuario.java
@@ -1,12 +1,16 @@
 package api.rest.model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Usuario implements Serializable{
@@ -19,6 +23,9 @@ public class Usuario implements Serializable{
 	private String login;
 	private String senha;
 	private String nome;
+	
+	@OneToMany(mappedBy = "usuario", orphanRemoval = true, cascade = CascadeType.ALL)
+	private List<Telefone> listaTelefones = new ArrayList<>();
 	
 	public Usuario() {
 	}
